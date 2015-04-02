acbf5f8b575fc1df14efc03397f0432fff79996b
codeMagnet <codeMagnet@outlook.com>
2015-04-02 21:59:30 +0100

    First push

---
+++ gth.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: codeM_000
+ * Date: 01-04-2015
+ * Time: 22:27
+ */
+?>
---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: codeM_000
+ * Date: 01-04-2015
+ * Time: 22:24
+ */
+echo("test");
+?>
